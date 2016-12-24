4680c2f61e7c8f155e4deab438f0366c9444c515
Kolbasyatin <zalex@zalex.com.ua>
2016-12-24 22:02:38 +0300

    Update client.php

--- client.php
+++ client.php
@@ -1,165 +1,339 @@
 <?php
 
-class OstrovokEchannelAPIClient {
-
-	protected $_auth_token = null;
-	protected $_private_token = null;
-
-	public function __construct($endpoint, $auth_token, $private_token) {
-		$this->_endpoint = $endpoint;
-		$this->_auth_token = $auth_token;
-		$this->_private_token = $private_token;
-	}
-
-	private function __signaturelizer($data) {
-		$is_list = false;
-		if (is_array($data)) {
-			if (count($data) > 0) {
-				if (is_int($data[0])) {
-					$is_list = true;
-				}
-			}
-		}
-
-		if (is_array($data) && !$is_list) {
-			ksort($data);
-			$tmp = array();
-			foreach($data as $key => $value) {
-				$tmp[] = array($this->__signaturelizer($key), $this->__signaturelizer($data[$key]));
-			}
-			$result = array();
-			foreach($tmp as $key => $value) {
-				$result[] = implode("=", $value);
-			}
-			return implode(";", $result);
-		} elseif (is_array($data) && $is_list) {
-			$result = array();
-			foreach($data as $value) {
-				$result[] = $this->__signaturelizer($value);
-			}
-			$result = implode(";", $result);
-			if (count($data) > 1) {
-				$result = ("[" . $result . "]");
-			}
-			return $result;
-		} elseif (is_bool($data)) {
-			return $data ? "true" : "false";
-		}
-
-		return (string)$data;
-	}
-
-	private function __getSignature(array $data, $private) {
-		$data['private'] = $private;
-		return md5($this->__signaturelizer($data));
-	}
-
-	private function __callGET($method_url, array $params) {
-		$params["token"] = $this->_auth_token;
-		$params["sign"] = $this->__getSignature($params, $this->_private_token);
-		$final_url = $this->_endpoint . $method_url . "?" . http_build_query($params) . "&";
-		return file_get_contents($final_url);
-	}
-
-	private function __callPUT($method_url, array $params) {
-		$GET_params = array();
-		$sign_params = $params;
-		$sign_params["token"] = $this->_auth_token;
-		$GET_params["token"] = $this->_auth_token;
-		$GET_params["sign"] = $this->__getSignature($sign_params, $this->_private_token);
-
-		$final_url = $this->_endpoint . $method_url . "?" . http_build_query($GET_params);
-		$curl = curl_init($final_url);
-
-		$data_json = json_encode($params);
-
-		curl_setopt($curl, CURLOPT_RETURN_TRANSFER, true);
-		curl_setopt($curl, CURLOPT_CUSTOM_REQUEST, "PUT");
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
-		curl_setopt($curl, CURLOP_HTTPHEADER, array(
-			'Content-Type: application/json',
-			'Content-Length: ' . strlen($data_json),
-		));
-
-		$response = curl_exec($curl);
-		curl_close($curl);
-		return $response;
-	}
-
-	private function __callPOST($method_url, array $params) {
-		$GET_params = array();
-		$sign_params = $params;
-		$sign_params["token"] = $this->_auth_token;
-		$GET_params["token"] = $this->_auth_token;
-		$GET_params["sign"] = $this->__getSignature($sign_params, $this->_private_token);
-
-		$final_url = $this->_endpoint . $method_url . "?" . http_build_query($GET_params);
-		$curl = curl_init($final_url);
-
-		$data_json = json_encode($params);
-
-		curl_setopt($curl, CURLOPT_RETURN_TRANSFER, true);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
-		curl_setopt($curl, CURLOP_HTTPHEADER, array(
-			'Content-Type: application/json',
-			'Content-Length: ' . strlen($data_json),
-		));
-
-		$response = curl_exec($curl);
-		curl_close($curl);
-		return $response;
-	}	
-
-	public function getHotels(array $params = array()) {
-		return $this->__callGET("hotels/", $params);
-	}
-
-	public function getRoomCategories(array $params = array()) {
-		return $this->__callGET("room_categories/", $params);		
-	}
-
-	public function getMealPlans(array $params = array()) {
-		return $this->__callGET("meal_plans/", $params);
-	}	
-
-	public function getOrders(array $params = array()) {
-		return $this->__callGET("orders/", $params);
-	}
-
-	public function getBookings(array $params = array()) {
-		return $this->__callGET("bookings/", $params);
-	}
-
-	public function getRNA($plan_date_start_at, $plan_date_end_at, array $params = array()) {
-		$params["plan_date_start_at"] = $plan_date_start_at;
-		$params["plan_date_end_at"] = $plan_date_end_at;
-		return $this->__callGET("rna/", $params);
-	}
-
-	public function updateRNA(array $params = array()) {
-		return $this->__callPUT("rna/", $params);
-	}
-
-	public function createRNA(array $params = array()) {
-		return $this->__callPOST("rna/", $params);
-	}
-
-	public function getRatePlans(array $params = array()) {
-		return $this->__callGET("rate_plans/", $params);
-	}
-
-	public function createRatePlan(array $rate_plan_params) {
-		return $this->__callPOST("rate_plans/", $rate_plan_params);
-	}
-
-	public function updateRatePlan($id, array $rate_plan_params) {
-		$rate_plan_params["id"] = $id;
-		return $this->__callPUT("rate_plans/", $rate_plan_params);
-	}
-}
+namespace MBH\Bundle\ChannelManagerBundle\Lib\Ostrovok;
 
 /**
- Usage:
-   $api_client = new OstrovokEchannelAPIClient("https://extratest.ostrovok.ru/", $auth_token, $private_token);
-   print $api_client->getHotels(); // returns json string
+ * Class OstrovokApiService
+ * @package MBH\Bundle\ChannelManagerBundle\Lib\Ostrovok
  */
+class OstrovokApiService
+{
+
+    /**
+     *
+     */
+    const API_URL = 'http://extrota-sandbox.ostrovok.ru/echannel/api/v0.1/';
+//    const API_URL = 'https://echannel.ostrovok.ru/echannel/api/v0.1/';
+
+    /**
+     * @var
+     */
+    protected $auth_token;
+
+    /**
+     * @var
+     */
+    protected $private_token;
+
+    /**
+     * OstrovokApiService constructor.
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->auth_token = $config['ostrovok']['username'];
+        $this->private_token = $config['ostrovok']['password'];
+    }
+
+    /**
+     * @param $data
+     * @return array|string
+     */
+    private function createSignatureString($data)
+    {
+        $is_list = false;
+        if (is_array($data) && count($data)) {
+            if ($data[0]??false) {
+                $is_list = true;
+            }
+        }
+
+        if (is_array($data) && !$is_list) {
+            ksort($data);
+            $tmp = array();
+            foreach ($data as $key => $value) {
+                $tmp[] = array($this->createSignatureString($key), $this->createSignatureString($data[$key]));
+            }
+            $result = array();
+            foreach ($tmp as $key => $value) {
+                $result[] = implode("=", $value);
+            }
+            return implode(";", $result);
+        } elseif (is_array($data) && $is_list) {
+            $result = array();
+            foreach ($data as $value) {
+                $result[] = $this->createSignatureString($value);
+            }
+            $result = implode(";", $result);
+            if (count($data) > 1) {
+                $result = ("[" . $result . "]");
+            }
+            return $result;
+        } elseif (is_bool($data)) {
+            return $data ? "true" : "false";
+        } elseif (is_null($data)) {
+            return 'None';
+        }
+
+        return (string)$data;
+    }
+
+    /**
+     * @param array $data
+     * @param $private
+     * @return string
+     */
+    private function getSignature(array $data, $private)
+    {
+        $data['private'] = $private;
+        $signatureString = $this->createSignatureString($data);
+        return md5($signatureString);
+    }
+
+    /**
+     * @param $api_method
+     * @param array $data
+     * @return mixed|string
+     * @throws OstrovokApiServiceException
+     */
+    private function callGet($api_method, array $data)
+    {
+        $data["token"] = $this->auth_token;
+        $data["sign"] = $this->getSignature($data, $this->private_token);
+        $request = self::API_URL . $api_method . "?" . http_build_query($data) . "&";
+
+        $response = file_get_contents($request);
+        if (!$response) {
+            throw new OstrovokApiServiceException('No returned request in callGet Method '.get_class($this));
+        }
+        $response = json_decode($response, true);
+        $this->checkErrors($response);
+        return $response;
+    }
+
+    /**
+     * @param $api_method
+     * @param array $data
+     * @param array $get_data
+     * @return mixed
+     */
+    private function callPUT($api_method, array $data, array $get_data = array())
+    {
+        return $this->makeCall("PUT", $api_method, $data, $get_data);
+    }
+
+    /**
+     * @param $api_method
+     * @param array $data
+     * @param array $get_data
+     * @return mixed
+     */
+    private function callPOST($api_method, array $data, array $get_data = array())
+    {
+        return $this->makeCall("POST", $api_method, $data, $get_data);
+    }
+
+    /**
+     * @param string $type
+     * @param string $api_method
+     * @param array $data
+     * @param array $get_data
+     * @return mixed
+     */
+    private function makeCall(string $type, string $api_method, array $data, array $get_data = array())
+    {
+        $signature_data = $data;
+        $signature_data["token"] = $this->auth_token;
+        $get_data["token"] = $this->auth_token;
+        $get_data["sign"] = $this->getSignature($signature_data, $this->private_token);
+
+        $final_url = self::API_URL . $api_method . "?" . http_build_query($get_data);
+        $curl = curl_init($final_url);
+
+        $data_json = json_encode($data);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $type);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_json),
+        ));
+
+        $response = json_decode(curl_exec($curl), true);
+        curl_close($curl);
+        $this->checkErrors($response);
+
+        return $response;
+    }
+
+    /**
+     * @param $response
+     * @throws OstrovokApiServiceException
+     */
+    private function checkErrors($response)
+    {
+        if (!empty($response['error'])) {
+            throw new OstrovokApiServiceException(
+                is_array($response['error']) ? http_build_query($response['error']) : $response['error']
+            );
+        };
+    }
+
+    /**
+     * @param array $data
+     * @return mixed|string
+     */
+    public function getHotels(array $data = array())
+    {
+        return $this->callGet("hotels/", $data);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function getRoomCategories(array $data = array())
+    {
+        $response = $this->callGet("room_categories/", $data);
+
+        return $response['room_categories'];
+    }
+
+    /**
+     * @param array $data
+     * @return mixed|string
+     */
+    public function getMealPlans(array $data = array())
+    {
+        return $this->callGet("meal_plans/", $data);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed|string
+     */
+    public function getOrders(array $data = array())
+    {
+        return $this->callGet("orders/", $data);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function getBookings(array $data = array())
+    {
+        $response = $this->callGet("bookings/", $data);
+        return $response['bookings'];
+    }
+
+    /**
+     * @param $plan_date_start_at
+     * @param $plan_date_end_at
+     * @param array $data
+     * @return mixed|string
+     */
+    public function getRNA($plan_date_start_at, $plan_date_end_at, array $data = array())
+    {
+        $data["plan_date_start_at"] = $plan_date_start_at;
+        $data["plan_date_end_at"] = $plan_date_end_at;
+        return $this->callGet("rna/", $data);
+    }
+
+    /**
+     * @param array $data
+     * @param bool $byKey
+     * @return array
+     */
+    public function getOccupancies(array $data = array(), bool $byKey = false)
+    {
+        $result = [];
+        $response = $this->callGet("occupancies/", $data);
+        $data = $response['occupancies'];
+        if ($byKey) {
+            foreach ($data as $occupancy) {
+                $result[$occupancy['id']] = $occupancy;
+            }
+        } else {
+            $result = $data;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $data
+     * @param bool $isShowDeleted
+     * @return array
+     */
+    public function getRatePlans(array $data = array(), $isShowDeleted = false)
+    {
+        $response = $this->callGet("rate_plans/", $data);
+
+        $rate_plans = [];
+        foreach ($response['rate_plans'] as $rate) {
+            if($rate['status'] === 'X' && !$isShowDeleted) continue;
+            $rate_plans[] = $rate;
+        }
+
+        return $rate_plans;
+
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function updateRNA(array $data = array())
+    {
+        return $this->callPUT("rna/", $data);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function createRNA(array $data = array())
+    {
+        return $this->callPOST("rna/", $data);
+    }
+
+    /**
+     * @param null $hotel
+     * @param null $room_category
+     * @param array $rate_plan_params
+     * @return mixed
+     */
+    public function createRatePlan($hotel = null, $room_category = null, array $rate_plan_params)
+    {
+        $get_data = array();
+        if (!is_null($hotel)) {
+            $get_data["hotel"] = $hotel;
+        }
+        if (!is_null($room_category)) {
+            $get_data["room_category"] = $room_category;
+        }
+        return $this->callPOST("rate_plans/", $rate_plan_params, $get_data);
+    }
+
+    /**
+     * @param $id
+     * @param null $hotel
+     * @param null $room_category
+     * @param array $rate_plan_params
+     * @return mixed
+     */
+    public function updateRatePlan($id, $hotel = null, $room_category = null, array $rate_plan_params = array())
+    {
+        $get_data = array();
+        $get_data["id"] = $id;
+        if (!is_null($hotel)) {
+            $get_data["hotel"] = $hotel;
+        }
+        if (!is_null($room_category)) {
+            $get_data["room_category"] = $room_category;
+        }
+        return $this->callPUT("rate_plans/", $rate_plan_params, $get_data);
+    }
+}
